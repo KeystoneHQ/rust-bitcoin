a5e454e4782095d00e758cfc59e56a6b98781702
yu <yu@keyst.one>
2024-01-02 17:30:22 +0800

    feat: add tests

--- bitcoin/tests/psbt.rs
+++ bitcoin/tests/psbt.rs
@@ -4,19 +4,23 @@
 use core::convert::TryFrom;
 use std::collections::BTreeMap;
 use std::str::FromStr;
+use secp256k1::{Signing, XOnlyPublicKey};
 
-use bitcoin::bip32::{ExtendedPrivKey, ExtendedPubKey, Fingerprint, IntoDerivationPath, KeySource};
+use bitcoin::bip32::{DerivationPath, ExtendedPrivKey, ExtendedPubKey, Fingerprint, IntoDerivationPath, KeySource};
 use bitcoin::blockdata::opcodes::OP_0;
 use bitcoin::blockdata::script;
 use bitcoin::consensus::encode::{deserialize, serialize_hex};
 use bitcoin::hashes::hex::FromHex;
-use bitcoin::psbt::{Psbt, PsbtSighashType};
+use bitcoin::psbt::{GetKey, Input, KeyRequest, Psbt, PsbtSighashType, SignError};
 use bitcoin::script::PushBytes;
 use bitcoin::secp256k1::{self, Secp256k1};
-use bitcoin::{
-    absolute, Amount, Denomination, Network, OutPoint, PrivateKey, PublicKey, ScriptBuf, Sequence,
-    Transaction, TxIn, TxOut, Witness,
-};
+use bitcoin::{absolute, Address, Amount, Denomination, Network, OutPoint, PrivateKey, PublicKey, ScriptBuf, Sequence, Transaction, transaction, TxIn, TxOut, Witness};
+use bitcoin::consensus::encode;
+use bitcoin::key::UntweakedPublicKey;
+use bitcoin_internals::hex::Case;
+use bitcoin_internals::hex::display::DisplayHex;
+use bitcoin::opcodes::all::OP_CHECKSIG;
+use bitcoin::taproot::{LeafVersion, TaprootBuilder};
 
 const NETWORK: Network = Network::Testnet;
 
@@ -470,4 +474,282 @@ fn finalize_psbt(mut psbt: Psbt) -> Psbt {
     psbt.inputs[1].bip32_derivation = BTreeMap::new();
 
     psbt
+}
+
+#[test]
+fn my_test_sign_taproot_in_psbt() {
+
+    macro_rules! test_hex_unwrap (($hex:expr) => (<Vec<u8> as $crate::FromHex>::from_hex($hex).unwrap()));
+
+    {
+        //abandon
+        //m/86'/1'/0'/0/0  dff1c8c2c016a572914b4c5adb8791d62b4768ae9d0a61be8ab94cf5038d7d90
+        //m/86'/1'/0'/0/1  1ede31b0e7e47c2afc65ffd158b1b1b9d3b752bba8fd117dc8b9e944a390e8d9
+        //m/86'/1'/0'/0/1  1fb777f1a6fb9b76724551f8bc8ad91b77f33b8c456d65d746035391d724922a
+    }
+
+    struct Keystore {
+        sk: PrivateKey,
+        mfp: Fingerprint,
+    }
+
+    impl GetKey for Keystore {
+        type Error = SignError;
+        fn get_key<C: Signing>(&self, key_request: KeyRequest, secp: &Secp256k1<C>) -> Result<Option<PrivateKey>, Self::Error> {
+            match key_request {
+                KeyRequest::Bip32((mfp, path)) => {
+                    if mfp == self.mfp {
+                        Ok(Some(self.sk.clone()))
+                    } else {
+                        Err(SignError::KeyNotFound)
+                    }
+                }
+                _ => Err(SignError::KeyNotFound)
+            }
+        }
+    }
+
+
+    let secp = &Secp256k1::<secp256k1::All>::gen_new();
+
+    //m/86'/1'/0'/0/0
+    let sk = "dff1c8c2c016a572914b4c5adb8791d62b4768ae9d0a61be8ab94cf5038d7d90";
+    let sk = test_hex_unwrap!(sk);
+    let sk = PrivateKey::from_slice(&sk, Network::Testnet).unwrap();
+    let x_only_pubkey = XOnlyPublicKey::from(sk.public_key(secp).inner);
+
+    let script1 = script::Builder::new()
+        .push_slice(x_only_pubkey.serialize())
+        .push_opcode(OP_CHECKSIG)
+        .into_script();
+
+    //m/86'/1'/0'/0/1
+    let sk = "1ede31b0e7e47c2afc65ffd158b1b1b9d3b752bba8fd117dc8b9e944a390e8d9";
+    let sk = test_hex_unwrap!(sk);
+    let sk = PrivateKey::from_slice(&sk, Network::Testnet).unwrap();
+    let x_only_pubkey = XOnlyPublicKey::from(sk.public_key(secp).inner);
+
+    let script2 = script::Builder::new()
+        .push_slice(x_only_pubkey.serialize())
+        .push_opcode(OP_CHECKSIG)
+        .into_script();
+
+    // //m/86'/1'/0'/0/2
+    let sk = "1fb777f1a6fb9b76724551f8bc8ad91b77f33b8c456d65d746035391d724922a";
+    let sk = test_hex_unwrap!(sk);
+    let sk = PrivateKey::from_slice(&sk, Network::Testnet).unwrap();
+    let x_only_pubkey = XOnlyPublicKey::from(sk.public_key(secp).inner);
+
+    let script3 = script::Builder::new()
+        .push_slice(x_only_pubkey.serialize())
+        .push_opcode(OP_CHECKSIG)
+        .into_script();
+
+    // use public key of path "m/86'/1'/0'/0/2" as internal key
+    let internal_key = x_only_pubkey.clone();
+
+    let builder = TaprootBuilder::new();
+    let builder = builder.add_leaf(2, script1.clone()).unwrap();
+    let builder = builder.add_leaf(2, script2.clone()).unwrap();
+    let builder = builder.add_leaf(1, script3.clone()).unwrap();
+    let tree_info = builder.finalize(&secp, internal_key).unwrap();
+    let output_key = tree_info.output_key();
+    // tb1pytee2mxz0f4fkrsqqws2lsgnkp8nrw2atjkjy2n9gahggsphr0gszaxxmv
+    let address = Address::p2tr_tweaked(output_key, Network::Testnet);
+    // m/86'/1'/0'/0/7
+    let receive_address = "tb1pyfv094rr0vk28lf8v9yx3veaacdzg26ztqk4ga84zucqqhafnn5q9my9rz";
+    let receive_address = Address::from_str(receive_address).unwrap();
+
+    //key path spend
+    {
+        let utxo_value = 6588;
+        let send_value = 6400;
+        let mfp = "73c5da0a";
+        let internal_key_path = "m/86'/1'/0'/0/2";
+        let out_puts = vec![
+            TxOut { value: send_value, script_pubkey: receive_address.payload.script_pubkey() },
+        ];
+        let prev_tx_id = "06980ca116f74c7845a897461dd0e1d15b114130176de5004957da516b4dee3a";
+
+        let tx1 = Transaction {
+            version: 2,
+            lock_time: absolute::LockTime::ZERO,
+            input: vec![TxIn {
+                previous_output: OutPoint { txid: prev_tx_id.parse().unwrap(), vout: 0 },
+                script_sig: ScriptBuf::new(),
+                sequence: Sequence(0xFFFFFFFF), // Ignore nSequence.
+                witness: Witness::default(),
+            }],
+            output: out_puts,
+        };
+
+        let mut psbt = Psbt::from_unsigned_tx(tx1).unwrap();
+        let mut origins = BTreeMap::new();
+        origins.insert(
+            internal_key.clone(),
+            (
+                vec![],
+                (
+                    Fingerprint::from_str(mfp).unwrap(),
+                    DerivationPath::from_str(internal_key_path).unwrap(),
+                ),
+            ),
+        );
+
+        let mut input = Input {
+            witness_utxo: {
+                let script_pubkey= address.payload.script_pubkey();
+                Some(TxOut { value: utxo_value, script_pubkey })
+            },
+            tap_key_origins: origins,
+            ..Default::default()
+        };
+        let ty = PsbtSighashType::from_str("SIGHASH_DEFAULT").unwrap();
+        input.sighash_type = Some(ty);
+        input.tap_internal_key = Some(internal_key);
+        input.tap_merkle_root = tree_info.merkle_root();
+        psbt.inputs = vec![input];
+
+
+        let sk = "1fb777f1a6fb9b76724551f8bc8ad91b77f33b8c456d65d746035391d724922a";
+        let sk = test_hex_unwrap!(sk);
+        let sk = PrivateKey::from_slice(&sk, Network::Testnet).unwrap();
+        let keystore = Keystore {
+            mfp: Fingerprint::from_str("73c5da0a").unwrap(),
+            sk,
+        };
+        psbt.sign(&keystore, secp);
+
+        let sig = "92864dc9e56b6260ecbd54ec16b94bb597a2e6be7cca0de89d75e17921e0e1528cba32dd04217175c237e1835b5db1c8b384401718514f9443dce933c6ba9c87";
+        assert_eq!(sig, psbt.inputs[0].tap_key_sig.unwrap().sig.to_string());
+
+        psbt.inputs.iter_mut().for_each(|input| {
+            let mut script_witness: Witness = Witness::new();
+            script_witness.push(input.tap_key_sig.unwrap().to_vec());
+            input.final_script_witness = Some(script_witness);
+
+            // Clear all the data fields as per the spec.
+            input.partial_sigs = BTreeMap::new();
+            input.sighash_type = None;
+            input.redeem_script = None;
+            input.witness_script = None;
+            input.bip32_derivation = BTreeMap::new();
+        });
+
+        let tx = psbt.extract_tx();
+        let tx_id = "5306516f2032d9f34c9f2f6d2b1b8ad2486ef1ba196d8d8d780e59773e48ad6d";
+        assert_eq!(tx_id, tx.txid().to_string());
+
+        let tx_bytes = "020000000001013aee4d6b51da574900e56d173041115bd1e1d01d4697a845784cf716a10c98060000000000ffffffff0100190000000000002251202258f2d4637b2ca3fd27614868b33dee1a242b42582d5474f51730005fa99ce8014092864dc9e56b6260ecbd54ec16b94bb597a2e6be7cca0de89d75e17921e0e1528cba32dd04217175c237e1835b5db1c8b384401718514f9443dce933c6ba9c8700000000";
+        let tx_hex = encode::serialize_hex(&tx);
+        assert_eq!(tx_bytes, tx_hex);
+    }
+
+    // script spend
+    {
+        let utxo_value = 6280;
+        let send_value = 6000;
+        let mfp = "73c5da0a";
+
+        let out_puts = vec![
+            TxOut { value: send_value, script_pubkey: receive_address.payload.script_pubkey() },
+        ];
+        let prev_tx_id = "9d7c6770fca57285babab60c51834cfcfd10ad302119cae842d7216b4ac9a376";
+        let tx1 = Transaction {
+            version: 2,
+            lock_time: absolute::LockTime::ZERO,
+            input: vec![TxIn {
+                previous_output: OutPoint { txid: prev_tx_id.parse().unwrap(), vout: 0 },
+                script_sig: ScriptBuf::new(),
+                sequence: bitcoin::Sequence(0xFFFFFFFF), // Ignore nSequence.
+                witness: Witness::default(),
+            }],
+            output: out_puts,
+        };
+
+        let mut psbt = Psbt::from_unsigned_tx(tx1).unwrap();
+
+        // //m/86'/1'/0'/0/1   signing key
+        let sk = "1ede31b0e7e47c2afc65ffd158b1b1b9d3b752bba8fd117dc8b9e944a390e8d9";
+        let sk = test_hex_unwrap!(sk);
+        let sk = PrivateKey::from_slice(&sk, Network::Testnet).unwrap();
+        let x_only_pubkey = XOnlyPublicKey::from(sk.public_key(secp).inner);
+
+        let use_script = script2.clone();
+        let use_path = "m/86'/1'/0'/0/1";
+        let mut origins = BTreeMap::new();
+        origins.insert(
+            x_only_pubkey.clone(),
+            (
+                vec![use_script.tapscript_leaf_hash()],
+                (
+                    Fingerprint::from_str(mfp).unwrap(),
+                    DerivationPath::from_str(use_path).unwrap(),
+                ),
+            ),
+        );
+
+        let mut tap_scripts = BTreeMap::new();
+        tap_scripts.insert(
+            tree_info.control_block(&(use_script.clone(), LeafVersion::TapScript)).unwrap(),
+            (use_script.clone(), LeafVersion::TapScript),
+        );
+
+        let mut input = Input {
+            witness_utxo: {
+                let script_pubkey= address.payload.script_pubkey();
+                Some(TxOut { value: utxo_value, script_pubkey })
+            },
+            tap_key_origins: origins,
+            tap_scripts,
+            ..Default::default()
+        };
+        let ty = PsbtSighashType::from_str("SIGHASH_ALL").unwrap();
+        input.sighash_type = Some(ty);
+        input.tap_internal_key = Some(internal_key);
+        input.tap_merkle_root = tree_info.merkle_root();
+        psbt.inputs = vec![input];
+
+
+        let keystore = Keystore {
+            mfp: Fingerprint::from_str(mfp).unwrap(),
+            sk,
+        };
+
+        psbt.sign(&keystore, secp);
+
+        let sig = "9c1466e1631a58c55fcb8642ce5f7896314f4b565d92c5c80b17aa9abf56d22e0b5e5dcbcfe836bbd7d409491f58aa9e1f68a491ef8f05eef62fb50ffac85727";
+        assert_eq!(sig, psbt.inputs[0].tap_script_sigs.get(&(x_only_pubkey, use_script.clone().tapscript_leaf_hash())).unwrap().sig.to_string());
+
+        psbt.inputs.iter_mut().for_each(|input| {
+            let mut script_witness: Witness = Witness::new();
+            for (_, signature) in input.tap_script_sigs.iter() {
+                script_witness.push(signature.to_vec());
+            }
+            for (control_block, (script, _)) in input.tap_scripts.iter() {
+                script_witness.push(script.to_bytes());
+                script_witness.push(control_block.serialize());
+            }
+            input.final_script_witness = Some(script_witness);
+
+            // Clear all the data fields as per the spec.
+            input.partial_sigs = BTreeMap::new();
+            input.sighash_type = None;
+            input.redeem_script = None;
+            input.witness_script = None;
+            input.bip32_derivation = BTreeMap::new();
+            input.tap_script_sigs = BTreeMap::new();
+            input.tap_scripts = BTreeMap::new();
+            input.tap_key_sig = None;
+        });
+
+
+        let tx = psbt.extract_tx();
+        let tx_id = "a51f723beffc810248809355ba9c9e4b39c6e55c08429f0aeaa79b73f18bc2a6";
+        assert_eq!(tx_id, tx.txid().to_string());
+
+        let tx_hex = encode::serialize_hex(&tx);
+        let tx_bytes = "0200000000010176a3c94a6b21d742e8ca192130ad10fdfc4c83510cb6baba8572a5fc70677c9d0000000000ffffffff0170170000000000002251202258f2d4637b2ca3fd27614868b33dee1a242b42582d5474f51730005fa99ce803419c1466e1631a58c55fcb8642ce5f7896314f4b565d92c5c80b17aa9abf56d22e0b5e5dcbcfe836bbd7d409491f58aa9e1f68a491ef8f05eef62fb50ffac857270122203058679f6d60b87ef921d98a2a9a1f1e0779dae27bedbd1cdb2f147a07835ac9ac61c1b68df382cad577d8304d5a8e640c3cb42d77c10016ab754caa4d6e68b6cb296d9b9d92a717ebeba858f75182936f0da5a7aecc434b0eebb2dc8a6af5409422ccf87f124e735a592a8ff390a68f6f05469ba8422e246dc78b0b57cd1576ffa98c00000000";
+        assert_eq!(tx_bytes, tx_hex);
+    }
 }
